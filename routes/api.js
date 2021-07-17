875fbf5f56d2dfdeb8c45c001f4eecb29f375227
Ashiemotto <ashheadley@gmail.com>
2021-07-17 11:00:31 -0400

    created backend

---
+++ routes/api.js
@@ -0,0 +1,72 @@
+//Imports............................................
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
+
+
+//GET//http://localhost:3001/api/workouts
+router.get('/api/workouts', (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {$sum: '$exercises.duration'},
+      },
+    },
+  ])
+    .then(workoutData => {
+        res.json(workoutData);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+
+//GET//http://localhost:3001/api/range
+router.get('/api/workouts/range', (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {$sum: '$exercises.duration'},
+      },
+    },
+  ])
+    .sort({_id: -1})
+    .limit(7)
+    .then(workoutData => {
+      res.json(workoutData);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+//POST//http://localhost:3001/api/workouts
+router.post('/api/workouts', ({body}, res) => {
+  Workout.create(body)
+    .then(workoutData => {
+      res.json(workoutData);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+//PUT//http://localhost:3001/api/workouts/:id
+router.put('/api/workouts/:id', ({body, params}, res) => {
+  Workout.findByIdAndUpdate(
+    {_id: params.id},
+    {$push: {exercises: body}},
+    {new: true})
+    .then(workoutData => {
+      res.json(workoutData);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+//Exports............................................
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan")
+
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(morgan('dev'))
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WorkoutDB", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/homeRoutes.js"));
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
